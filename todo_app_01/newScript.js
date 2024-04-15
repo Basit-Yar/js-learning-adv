b0e749a293d296e895145d731eeb95a7c37c53ba
SyedBasitYar <basityar6@gmail.com>
2024-04-15 12:30:56 +0530

    added search freature and delete all todos feature

--- todo_app_01/newScript.js
+++ todo_app_01/newScript.js
@@ -2,6 +2,8 @@ const todo = document.querySelector("#task");
 const dueTime = document.querySelector("#f-time");
 const addBtn = document.getElementById("add-todo");
 const tableBody = document.querySelector("#table-body")
+const searchBox = document.querySelector("#search");
+const deleteAllBtn = document.querySelector("#delete-all-todo");
 
 const addBtnDisplayValue = addBtn.value;
 let editIndexNum = null;
@@ -36,16 +38,16 @@ if (retrieveLocalStorageTodoObjs != null) {
 const addNewTodoTask = (event) => {
     event.preventDefault();
     let currentTime = formattedDateTime(new Date());
-    
+
     inputtedTaskObj = {
         task: todo.value,
         dueTime: dueTime.value,
         createdAt: currentTime
     }
 
-    
+
     if (editIndexNum != null) {
-        const updatedTaskObj = {...inputtedTaskObj, createdAt : taskCreationRealTime}
+        const updatedTaskObj = { ...inputtedTaskObj, createdAt: taskCreationRealTime }
 
         todoStorageArray.splice(editIndexNum, 1, updatedTaskObj);
         editIndexNum = null;
@@ -82,7 +84,7 @@ const editTodoTask = (e) => {
 
     if (isConfirmed) {
         let selectedRow = e.target.closest("tr");
-        let indexNum = selectedRow.querySelector(".s-no").innerHTML -1;
+        let indexNum = selectedRow.querySelector(".s-no").innerHTML - 1;
         // let task = selectedRow.querySelector(".todo-value").innerHTML;
         let task = todoStorageArray[indexNum].task;
         // let dueAt = selectedRow.querySelector(".todo-due-time").innerHTML;
@@ -101,7 +103,7 @@ const deleteTodoTask = (e) => {
     if (e.target.innerHTML === "Delete") {
         let indexNum = e.target.closest("tr").querySelector(".s-no").innerHTML;
         const isConfirmed = confirm(`You are going to delete this task! Sno. ${indexNum}\nDo you want to delete this task?`);
-        if (isConfirmed){
+        if (isConfirmed) {
             todoStorageArray.splice(indexNum - 1, 1);
             localStorage.setItem("todos", JSON.stringify(todoStorageArray));
             displayAllTodoTask()
@@ -114,3 +116,33 @@ addBtn.addEventListener("click", addNewTodoTask);
 tableBody.addEventListener("click", editTodoTask);
 tableBody.addEventListener("click", deleteTodoTask);
 displayAllTodoTask();
+
+
+// ================================== Search Feature ==================================
+
+let allTr = tableBody.querySelectorAll("#table-body tr");
+
+searchBox.addEventListener("input", () => {
+    const searchValue = searchBox.value.toLowerCase();
+    // console.log(searchValue);
+    tableBody.innerHTML = "";
+    allTr.forEach(tr => {
+        let todoValue = tr.querySelector(".todo-value").innerHTML.toLowerCase();
+        console.log(todoValue);
+        const isFound = todoValue.indexOf(searchBox.value);
+        if(isFound >= 0) {
+            tableBody.appendChild(tr);
+        }
+    });
+})
+
+// ============= delete all todos ================
+
+deleteAllBtn.addEventListener("click", () => {
+    const isConfirmed = confirm("Do you really want to delete all the todos ?");
+    if (isConfirmed) {
+        localStorage.removeItem("todos");
+        todoStorageArray = [];
+        displayAllTodoTask();
+    }
+})
